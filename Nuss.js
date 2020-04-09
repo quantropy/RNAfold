ddb30829cc6d5db12d7db5e375e3dd4030b8880a
quantropy <quantropy@users.noreply.github.com>
2020-04-09 10:19:45 +0100

    Update Nuss.js

--- Nuss.js
+++ Nuss.js
@@ -1,4 +1,13 @@
 //Kravchenko
-var sequence='GGCCGAAAAUCGAAGGGAAAAAAAACCGCC'
-for (i=0;i<30;i++)
-N[i][j]
+function ispair(i,j){
+}
+var sequence='GGCCGAAAAUCGAAGGGAAAAAAAACCGCC';
+var aseq=sequence.split("");
+for (i=0;i<30;i++){
+N[i][i]=0;
+N[i][i+1]=0;
+N[i][i+2]=0;
+N[i][i+3]=0;}
+
+
+}
